b5e238732520296e10ad5cfe67117adb0fc2caf7
barmem14689 <slslclalal@gmail.com>
2025-02-06 18:36:08 +0300

    Create task6.cpp

---
+++ task6.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>  
+#include <string>  
+using namespace std;
+
+int main() {
+    int x = 0;   
+    int y = 0;   
+
+    string direction;  
+    int steps;  
+
+      
+    while (true) {
+        cout << "enter direction(N,E,S,W) or STOP to end programm: ";
+        cin >> direction;   
+        if (direction == "STOP") {
+            break;  
+        }
+        cout << "enter number of steps: ";
+        cin >> steps;     
+        if (direction == "N") {
+            y += steps;  
+        }
+        else if (direction == "E") {
+            x += steps;   
+        }
+        else if (direction == "S") {
+            y -= steps;  
+        }
+        else if (direction == "W") {
+            x -= steps;  
+        }
+        else {
+            cout << "direction isnt right try again." << endl;
+        }
+    }
+
+      
+    cout << "finally point: (" << x << ", " << y << ")" << endl;
+}//наверное можно без стринга, но я не знаю как
