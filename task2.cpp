61fead7f220445f418f114689db445fd847e947a
barmem14689 <slslclalal@gmail.com>
2025-02-06 18:27:24 +0300

    tasks

---
+++ task2.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	char a;
+	cin >> a;
+	char c = a - 32;
+	cout << c;
+}
---
+++ task5.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    int c;
+    int z;
+    int s = 0;
+    cout << "enter number of customers ";
+    cin >> a;
+    int arr[a];
+    cout << "enter age of customers ";
+    for (int i = 0; i < a; i++)
+    {
+        cin >> arr[i];
+        if (arr[i] > arr[i - 1])
+        {
+            c = arr[i];
+        }
+        else if (arr[i] < arr[i - 1])
+        {
+            z = arr[i];
+        }
+        s = s + arr[i];
+    }
+    cout << "the oldest: " << c << endl;
+    cout << "the youngest: " << z << endl;
+    cout << "average: " << s / a << endl;
+    //на vs не завелся, но в онлайн компиляторе заработал
+}
+
